15ddfa6aaaa01dc10cc52a4768d045292e628e0d
Ryan Taheri <sailantht@gmail.com>
2021-11-12 15:18:41 +0330

    Article Details

--- services/index.js
+++ services/index.js
@@ -38,6 +38,43 @@ export const getPosts = async () => {
     return result.postsConnection.edges;
 };
 
+
+export const getPostDetails = async (slug) => {
+    const query = gql`
+        # we are accepting a slug that will be a string
+        query GetPostDetails($slug: String!) {
+            post(where: {slug: $slug}){
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                content {
+                    raw
+                }
+                categories {
+                    name
+                    slug
+                }
+            }
+        }
+    `;
+
+    const result = await request(graphqlAPI, query, {slug});
+    return result.post;
+};
+
+
 export const getRecentPosts = async () => {
     const query = gql`
         query GetPostDetails {
@@ -61,7 +98,7 @@ export const getRecentPosts = async () => {
 }
 
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
         query GetPostDetails($slug:String!, $categories:[String!]) {
             posts(
@@ -81,7 +118,7 @@ export const getSimilarPosts = async () => {
         }
 
     `
-    const result = await request(graphqlAPI, query);
+    const result = await request(graphqlAPI, query, {categories, slug});
     return result.posts;
 }
 
